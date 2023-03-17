04a600adcc50f27c6588f22757a9b3b6f63dc463
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-17 08:38:14 -0300

    Configs

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "history",
+    title: "História da Cachaça",
+  },
+  {
+    id: "about",
+    title: "Quem somos",
+  },
+  {
+    id: "contact",
+    title: "Contato",
+  },
+];
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        golden: "#DCCA87",
+        "black-100": "#111",
+        "black-200": "#1d1d1d",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/cachaca.png')",
+      },
+    },
   },
   plugins: [],
-}
+};
