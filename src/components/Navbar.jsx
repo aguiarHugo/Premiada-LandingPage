3a2e2835c73524dac1d5145f5be73e948919ba25
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-17 09:52:40 -0300

    Styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles/styles";
 import { navLinks } from "../constants";
 import logo from '../assets/logo.png'
+import menu from '../assets/menu.svg'
+import close from '../assets/close.svg'
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -22,9 +24,58 @@ const Navbar = () => {
         >
           <div className="flex items-center text-golden">
             <img src={logo} alt="logo" className="w-20 h-20 object-contain"/>
-            <p>Cachaça Premiada</p>
+            <p className="font-great-vibes text-3xl">Cachaça Premiada</p>
           </div>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                ? 'text-golden' 
+                : 'text-primary'
+              }text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a className="hover:text-golden" href={`#${link.id}`}>
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img 
+            src={toggle ? close : menu} 
+            alt="menu"
+            className="w-[24px] h-[24px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 bg-black-200 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                  active === link.title
+                  ? 'text-golden' 
+                  : 'text-primary'
+                  }text-[16px] font-poppins font-medium cursor-pointer`}
+                  onClick={() =>{ 
+                    setToggle(!toggle)
+                    setActive(link.title)
+                  }}
+                >
+                  <a className="hover:text-golden" href={`#${link.id}`}>
+                    {link.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
