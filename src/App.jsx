d68d47e56f0d9554295c81da0726beaf71824ec2
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-17 09:08:22 -0300

    New imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { BrowserRouter } from "react-router-dom"
 
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 function App() {
 
   return (
-    <div className="App">
-      oi
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-black-200">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
