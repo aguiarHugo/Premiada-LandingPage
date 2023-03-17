4a4abff108c135303c5dc74879c9cf31e8375a3d
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-17 10:24:33 -0300

    Imported History component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom"
 
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import History from "./components/History"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Hero />
         </div>
+        <History />
       </div>
     </BrowserRouter>
   )
