1805a8dac09816af98a68ec8db8e7e7f4c499189
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-19 11:12:03 -0300

    deleted home link

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,13 @@
+import logo from '../assets/logo.png'
+
 export const navLinks = [
-  {
-    id: "home",
-    title: "Home",
-  },
   {
     id: "history",
     title: "História da Cachaça",
+  },
+  {
+    id: "production",
+    title: "Como é feita"
   },
   {
     id: "about",
@@ -14,5 +16,89 @@ export const navLinks = [
   {
     id: "contact",
     title: "Contato",
+  },
+]
+
+export const steps = [
+  {
+    title: "A cana de açúcar e a colheita:",
+    icon: logo,
+    iconBg: "#000",
+    date: "1° Passo",
+    points: [
+      "Por ser uma planta tropical que cresce bem em temperaturas entre 18ºC a 35º, mas isso não quer dizer que você não deva se atentar às condições de solo e climáticas da região em que sua plantação se encontra, para que você possa tirar mais proveito de sua variedade. Aliás, existem diversas variedades de cana, como, por exemplo, a SP801816, a SP 803280, a RB 867515, dentre outras.",
+      "A colheita pode ser feita por máquinas, ou à mão. No caso das máquinas, fique atento aos processos de higienização para que a cana não seja contaminada. Além disso, lembre-se de que a cana deve ser moída em, no máximo, 24 horas após ser recolhida."
+    ],
+  },
+  {
+    title: "Moagem:",
+    icon: logo,
+    iconBg: "#000",
+    date: "2° Passo",
+    points: [
+      "Nesta etapa, a garapa é extraída da cana e o bagaço é separado do caldo de cana. Preste atenção a regulagem de sua moenda!",
+      "Garapa é o nome dado ao caldo extraído da cana-de-açúcar."
+    ],
+  },
+  {
+    title: "Filtragem:",
+    icon: logo,
+    iconBg: "#000",
+    date: "3° Passo",
+    points: [
+      "Aqui o caldo é filtrado por uma peneira e por decantação, deixando para trás o bagacilho e quaisquer tipos de outros resíduos. Vale lembrar que esta etapa é de extrema importância uma vez que, qualquer elemento indesejável que seja mau filtrado e prossiga nas etapas seguintes pode prejudicar a qualidade do produto final.",
+    ],
+  },
+  {
+    title: "Redução do Brix:",
+    icon: logo,
+    iconBg: "#000",
+    date: "4° Passo",
+    points: [
+      "Após a filtragem, chega a hora de reduzir o brix da cana. Para isso, basta adicionar água ao caldo de cana. Geralmente, o brix vai de 18-24 para 14-16."
+    ],
+  },
+  {
+    title: "Fermentação:",
+    icon: logo,
+    iconBg: "#000",
+    date: "5° Passo",
+    points: [
+      "Com a duração entre 16 e 24 horas, o processo de fermentação consiste na transformação do açúcar em álcool e gás carbônico por micro-organismos. Esta etapa exige bastante atenção, principalmente com relação à sua temperatura, que deve variar entre 28º e 32º.",
+      "Ao final desse processo, depois de todo açúcar ter sido convertido em álcool, a solução final (vinho) deve estar com uma concentração entre 7 – 9 % de álcool."
+    ],
+  },
+  {
+    title: "Destilação:",
+    icon: logo,
+    iconBg: "#000",
+    date: "6° Passo",
+    points: [
+      "Nesta etapa podem ser usados alambiques de diferentes tamanhos, formatos, materiais e volumes para destilar o vinho da cana. O aquecimento do alambique pode ser feito de forma direta (fogo direto na panela) ou indireta (aquecimento por vapor).",
+      "Na destilação de cachaça industrial, a coluna utilizada é de inox. Já na produção artesanal são utilizados alambiques de cobre. De acordo com os especialistas da área, o cobre ajuda a reduzir o teor de compostos sulfurados na cachaça, evitando a presença de odores desagradáveis.",
+      "Durante a etapa inteira da destilação em alambique artesanal, são separados a cabeça, coração e cauda, sendo o coração o único ideal para consumo. Os outros dois podem ser reaproveitados para a produção de álcool combustível."
+    ],
+  },
+  {
+    title: "Armazenamento e Envelhecimento:",
+    icon: logo,
+    iconBg: "#000",
+    date: "7° Passo",
+    points: [
+      "A cachaça ideal para consumo deve ser armazenada e envelhecida por, pelo menos, seis meses,  em dornas de inox, barris de madeira ou até mesmo recipientes de vidro. O que conta é que o recipiente deve estar devidamente limpo para que não haja risco de contaminação.",
+      "Algumas das madeiras mais usadas são: carvalho, amburana, bálsamo, garapeira, freijó, jequitibá, jatobá, castanheira, etc."
+    ],
+  },
+  {
+    title: "Engarrafamento:",
+    icon: logo,
+    iconBg: "#000",
+    date: "8° Passo",
+    points: [
+      "Após a etapa de armazenamento e envelhecimento, a cachaça será diluída e padronizada para, então, ser engarrafada.",
+      "Ao final desse processo, depois de todo açúcar ter sido convertido em álcool, a solução final (vinho) deve estar com uma concentração entre 7 – 9 % de álcool.",
+      "A padronização é uma etapa crucial em seu processo.",
+      "Vale lembrar que, para seu produto ser chamado de “Cachaça”, ele deve ter o teor alcoólico entre 38º e 48º."
+    ],
   },
 ];
