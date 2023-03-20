ffc3148cfc749a67741ffb64579bd588408aa9de
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-19 21:19:07 -0300

    Styled

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-black px-5 py-4 flex flex-wrap justify-between'>
+      <div>
+        <h2 className='font-bold'><span className='text-golden text-[12px]'>©</span>Cachaça Premiada</h2>
+      </div>
+      <div>
+        <p className='font-bold font-poppins'>2023</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
